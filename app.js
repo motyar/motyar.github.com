428e1587e2fa0ae72739b1a311c70a2ea39a1312
Motyar <dharmmotyar@gmail.com>
2014-04-10 16:25:16 +0530

    Updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
     var edit = url.match(/[&\?]edit=([\w\/-]+)/)[1];
     if(edit!=""){//Redirect to github edit
         //window.location.href = 'http://github.com';
-
+        //TODO if file is index.html and filename doest exits
         var editUrl = url.replace("//","//github.com/motyar/motyar.github.com/").replace("motyar.github.io/","edit/master/").replace("?edit="+edit,"");
         window.location.href = editUrl; 
     }
