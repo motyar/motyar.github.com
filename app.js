b2b892d30af4cae9a3c73a2ebc7ec98979c95fbf
Motyar <dharmmotyar@gmail.com>
2014-04-10 16:04:34 +0530

    Updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
     if(edit!=""){//Redirect to github edit
         //window.location.href = 'http://github.com';
 
-        var editUrl = url.replace("//","//github.com/motyar/motyar.github.com/").replace(".io/","ol");
+        var editUrl = url.replace("//","//github.com/motyar/motyar.github.com/").replace("motyar.gihub.io/","/edit/master/").replace("?edit="+edit,"");
         alert(editUrl);
     
     }
