5ee91ba815dbe756a761453644a8d8d9136a4e65
Motyar <dharmmotyar@gmail.com>
2014-04-10 15:59:37 +0530

    Updated

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
     var edit = url.match(/[&\?]edit=([\w\/-]+)/)[1];
     if(edit!=""){//Redirect to github edit
         window.location.href = 'http://github.com';
+
+        var editUrl = url.replace("//","//github.com/motyar/motyar.github.com/");
+        alert(editUrl);
     
     }
 })();
